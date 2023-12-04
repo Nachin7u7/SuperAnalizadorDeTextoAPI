2154b999c1bf08da8e28bb9656a3df651782fa05
Ignacio <ignacio.valencia.sauma@gmail.com>
2023-12-04 01:23:39 -0400

    little bug goes bye bye :D

--- main.py
+++ main.py
@@ -5,113 +5,87 @@ import pandas as pd
 
 app = FastAPI()
 
-# Estructura de datos para la solicitud POST /sentiment
-
 
 class SentimentInput(BaseModel):
     text: str
-
-# Estructura de datos para la solicitud POST /analysis
 
 
 class AnalysisInput(BaseModel):
     text: str
 
 
-# Datos de modelos alojados
 models_info = {
     "sentiment": {"name": "Sentiment Analysis Model", "type": "NLP"},
     "nlp": {"name": "Spacy NLP Model", "type": "NLP"},
 }
 
-# Lista para almacenar los reportes de inferencia
 inference_reports = []
 
 
-# Endpoint GET /status
 @app.get("/status")
 def read_status():
     return {"service": "Running", "models": models_info}
 
 
-# Endpoint POST /sentiment
 @app.post("/sentiment")
 def analyze_sentiment(input_data: SentimentInput):
     start_time = time.time()
-    # Lógica para inferencia de análisis de sentimiento aquí (reemplazar con tu implementación real)
-    sentiment_value = 0.5  # Placeholder para el valor de sentimiento
+    sentiment_value = 0.5
     execution_time = time.time() - start_time
 
-    # Construir respuesta
     response_data = {
         "sentiment_value": sentiment_value,
         "execution_time": execution_time,
         "model_info": models_info["sentiment"],
     }
 
-    # Agregar a informes de inferencia
     inference_reports.append(response_data)
 
     return response_data
 
 
-# Endpoint POST /analysis
 @app.post("/analysis")
 def analyze_text(input_data: AnalysisInput):
     start_time = time.time()
-    # Lógica para inferencia de análisis de texto aquí (reemplazar con tu implementación real)
-    analysis_value = 0.7  # Placeholder para el valor de análisis
+    analysis_value = 0.7
     execution_time = time.time() - start_time
 
-    # Construir respuesta
     response_data = {
         "analysis_value": analysis_value,
         "execution_time": execution_time,
         "model_info": models_info["nlp"],
     }
 
-    # Agregar a informes de inferencia
     inference_reports.append(response_data)
 
     return response_data
 
 
-# Endpoint GET /reports
 @app.get("/reports")
 def generate_reports():
-    # Crear un DataFrame a partir de los informes de inferencia
+
     df = pd.DataFrame(inference_reports)
-    # Generar archivo CSV
+
     csv_data = df.to_csv(index=False)
 
-    # Devolver el contenido del archivo CSV como respuesta
     return {"csv_data": csv_data}
-
-# Estructura de datos para la solicitud POST /analysis_v2
 
 
 class AnalysisV2Input(BaseModel):
     text: str
     prompt: str
 
-# Endpoint POST /analysis_v2
-
 
 @app.post("/analysis_v2")
 def analyze_text_v2(input_data: AnalysisV2Input):
     start_time = time.time()
 
-    # Lógica para inferencia de análisis de texto con características adicionales
-    # (POS tagging, NER, sentimiento basado en prompt, ejecución de LLM de OpenAI)
-
-    # Placeholder para los valores de análisis
-    pos_tags = ["NOUN", "VERB", "ADJ"]  # Placeholder para POS tagging
-    ner_entities = [{"text": "Apple", "label": "ORG"}]  # Placeholder para NER
-    sentiment_prompt = 0.8  # Placeholder para sentimiento basado en prompt
-    lm_output = "Generated text from LLM"  # Placeholder para la salida del LLM
+    pos_tags = ["NOUN", "VERB", "ADJ"]
+    ner_entities = [{"text": "Apple", "label": "ORG"}]
+    sentiment_prompt = 0.8
+    lm_output = "Generated text from LLM"
     execution_time = time.time() - start_time
 
-    # Construir respuesta
     response_data = {
         "pos_tags": pos_tags,
         "ner_entities": ner_entities,
@@ -121,7 +95,6 @@ def analyze_text_v2(input_data: AnalysisV2Input):
         "model_info": {"name": "Combined Analysis Model", "type": "NLP"},
     }
 
-    # Agregar a informes de inferencia
     inference_reports.append(response_data)
 
     return response_data
@@ -130,5 +103,4 @@ def analyze_text_v2(input_data: AnalysisV2Input):
 if __name__ == "__main__":
     import uvicorn
 
-    # Para probar la aplicación, ejecutar con uvicorn
     uvicorn.run(app, host="127.0.0.1", port=8000)
